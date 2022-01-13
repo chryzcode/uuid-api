98aabe7caaf3a881c477bf51a206af2ae4d3dc25
Alaba Olanrewaju <alabaolanrewaju13@gmail.com>
2022-01-13 17:16:37 +0100

    push

--- uuid_project/settings.py
+++ uuid_project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-dd8t5h3wb#e%^ytd_iy=^0fm7#dndi)ba#uo&&l1_1&fz_)ry%'
+SECRET_KEY = os.str(os.environ.get('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'uuiid_api',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
